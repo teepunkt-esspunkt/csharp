e1a12b3631ab51b93f2ed8e8bbc6d8abdc98f0f2
teepunkt-esspunkt <tsaleh@web.de>
2023-12-14 06:24:42 +0100

    domo

--- Abschnitt-1/ProjektArbeit/Privatkunde.cs
+++ Abschnitt-1/ProjektArbeit/Privatkunde.cs
@@ -7,12 +7,15 @@ using System.Threading.Tasks;
 
 namespace ProjektArbeit
 {
+    // Privatkunden Klasse, Erbt von Kunde.
     internal class Privatkunde : Kunde
     {
+        // Attribute
         private string vorname;
         private string nachname;
         private DateTime geburtsdatum;
 
+        // Constructor
         public Privatkunde(string vorname, string nachname, DateTime geburtsdatum, string telefonnummer, string email, Adresse adresse, int anzahlKonten) : base(telefonnummer, email, adresse, anzahlKonten)
         {
             this.vorname = vorname;
@@ -21,14 +24,21 @@ namespace ProjektArbeit
 
         }
 
+        // Methode zum Privatkunden Anlegen
         internal static void PrivatkundeAnlegen()
         {
+            // Leerzeile damit es nicht so an dem vorangegangenen Text klebt
+            Console.WriteLine("");
 
+            //Regex Pattern fuer Vor- und Nachname
             string  pattern = @"^[A-Za-zÖÄÜöäü]{2,}(?:[-\s'][A-Za-zÖÄÜöäü]+)?$";
             Regex regexName = new Regex(pattern);
 
+            // Attribute
             string vorname;
             string nachname;
+
+            //Vorname (Mit Throw New ArgumentException, welcher Weg ist besser?)
             while(true)
             { 
                 Console.Write("Bitte Vornamen eingeben: ");
@@ -38,15 +48,22 @@ namespace ProjektArbeit
                     bool istGueltig;
                     string eingabe = Console.ReadLine();
                     if (regexName.IsMatch(eingabe))
+                    { 
                         vorname = eingabe;
-                    break;
-                    
+                        break;
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+
                 }
                 catch(ArgumentException ae)
                 {
                     Console.WriteLine("Name darf nur aus alphabetischen Zeichen bestehen, und muss mindestens 2 Zeichen lang sein");
                 }
             }
+            // Nachname (Ohne Throw New... Welcher Weg ist besser?)
             while (true)
             {
                 Console.Write("Bitte Nachname eingeben: ");
@@ -56,9 +73,15 @@ namespace ProjektArbeit
                     bool istGueltig;
                     string eingabe = Console.ReadLine();
                     if (regexName.IsMatch(eingabe))
+                    { 
                         nachname = eingabe;
-                    break;
-
+                        break;
+                    } 
+                        // ist das hier ueberhaupt noetig? zZz
+                    //else
+                    //{
+                    //    throw new ArgumentException();
+                    //}
                 }
                 catch (ArgumentException ae)
                 {
@@ -66,27 +89,38 @@ namespace ProjektArbeit
                 }
             }
 
+            // Geburtsdatum
+            //while (true)
+            //{
+                Console.Write("Bitte Geburtsdatum im Format \"JJJJ.MM.TT\"eingeben: ");
+                string geburtsdatum = Console.ReadLine();
 
-            Console.Write("Bitte Geburtsdatum im Format \"JJJJ.MM.TT\"eingeben: ");
-            string geburtsdatum = Console.ReadLine();
-            //Console.Write("Bitte das Jahr der Geburt eingeben: ");
-            //string geburtsjahr = Console.ReadLine();
-            //Console.Write("Bitte den Monat der Geburt eingeben: ");
-            //string geburtsmonat = Console.ReadLine();
-            //Console.Write("Bitte den Tag der Geburt eingeben: ");
-            //string geburtstag = Console.ReadLine();
+
+            //}
+
+            // TODO: +49 vorbelegen, nur zahlen und leerzeichen und 1 / oder -
             Console.Write("Bitte Telefonnummer eingeben: ");
             string telefonnummer = Console.ReadLine();
+            // Heere we GooO
             Console.Write("Bitte EMail eingeben: ");
             string email = Console.ReadLine();
+
+            //wie name
             Console.Write("Bitte Strasse eingeben: ");
             string strasse = Console.ReadLine();
+
+            // ezpz
             Console.Write("Bitte Hausnummer eingeben: ");
             string hsnr = Console.ReadLine();
+
+            //ezpziest
             Console.Write("Bitte Postleitzahl eingeben: ");
             string plz = Console.ReadLine();
+
+            // wie name(?)
             Console.Write("Bitte Ort eingeben: ");
             string ort = Console.ReadLine();
+            // _
             Console.Write("Bitte Anzahl der gewünschten Konten eingeben: ");
             string anzahl = Console.ReadLine();
 
